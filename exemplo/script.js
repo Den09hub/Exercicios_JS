b10a1172ef53662a429b89b1abc7eba7345f61e3
Den09hub <denyel.1005@gmail.com>
2025-03-14 16:55:45 -0400

    task1

---
+++ exemplo/script.js
@@ -0,0 +1,68 @@
+// alert("Olá Mundo!");
+// confirm("Confirma a conclusão");
+// prompt("Qual seu nome?");
+
+// let x = BigInt("12323131231");
+
+// console.log(x);
+
+// let nota = 10 / 3;
+
+// nota = String(nota);
+// nota = nota.replace(".",",");
+
+// console.log(nota);
+
+
+// nome = "Denyel";
+
+// console.log("Meu nome é " + "Denyel");
+// console.log(`Meu nome é ${nome}`);
+
+// let media = 8;
+
+// let res = media >= 7 ? 'Aprovado' : 'Reprovado';
+
+// console.log(res);
+
+// let text = "CWIFJWOECKFB";
+
+// let tamanho = text.length;
+
+// console.log(tamanho);
+
+// let text = "Hello World";
+// let char = text.charAt(1);
+
+// console.log(char);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7, 13);
+
+// console.log(part);
+
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase();
+
+// console.log(text2);
+
+// let ex1 = "Olá Mundo!";
+// let ex2 = ex1.toLowerCase();
+
+// console.log(ex2);
+
+// let idade = '18';
+// idade = parseInt(idade);
+// console.log(idade);
+
+// let salario = '2500';
+// salario = parseFloat(salario);
+// console.log(salario);
+
+function myFunction(p1, p2){
+    return p1 * p2;
+}
+
+console.log(myFunction(50, 25));
+
+
