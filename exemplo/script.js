b36655ec3cc8d2cf1e58bda1f3bce5b7f69f1532
Den09hub <denyel.1005@gmail.com>
2025-03-17 17:17:05 -0400

    task2

--- exemplo/script.js
+++ exemplo/script.js
@@ -59,10 +59,98 @@
 // salario = parseFloat(salario);
 // console.log(salario);
 
-function myFunction(p1, p2){
-    return p1 * p2;
-}
+// function myFunction(p1, p2){
+//     return p1 * p2;
+// }
 
-console.log(myFunction(50, 25));
+// console.log(myFunction(50, 25));
+
+// let idade = 18;
+
+// if (idade>=18){
+//     console.log('Maior de idade');
+// } else {
+//     console.log('Menor de idade');
+// }
+
+// let idade = 16;
+
+// if (idade <= 16){
+//     console.log('Adolescente');
+// } else if(idade > 16 && idade < 42){
+//     console.log('Adulto');
+// } else {
+//     console.log('Idoso');
+// }
+
+// menu = 0;
+
+// switch(menu){
+//     case 0:
+//         alert('Sair')
+//         break;
+
+//     case 1:
+//         alert('Saldo')
+//         break;
+
+//     default:
+//         alert('Valor Incálido');
+// }
+
+// switch(new Date().getDay()){
+//     case 0:
+//         day = 'Domingo'
+//         break;
+
+//     case 1:
+//         day = 'Segunda'
+//         break;
+
+//     case 2:
+//         day = 'Terça'
+//         break;
+
+//     case 3:
+//         day = 'Quarta'
+//         break;
+
+//     case 4:
+//         day = 'Quinta'
+//         break;
+
+//     case 5:
+//         day = 'Sexta'
+//         break;
+
+//     case 6:
+//         day = 'Sábado'
+//         break;
+// }
+
+// alert(day);
+
+// let i = 0;
+
+// while(i < 10){
+//     let text;
+//     text = 'O número é ' + i;
+//     console.log(text);
+//     i++;
+// }
+
+// let i = 5;
+// do {
+//     let text = 'The number is ' + i;
+//     i++;
+//     console.log(text);
+// }while(i<10)
+
+// let num = 0;
+
+// for(num = 0; num < 9; num++){
+//     console.log(num);
+// }
+
 
 
