6e770cf1a5771f880f7c84698fe91ea5836861a0
Den09hub <denyel.1005@gmail.com>
2025-03-24 17:07:54 -0400

    task10

---
+++ exemplo/script_validacao.js
@@ -0,0 +1,24 @@
+// document.write(/[az]/i.test("AZ"))
+// document.write(/love/i.test("LovE"))
+// document.write(/\x/.test('w'))
+
+// document.write(/^\d{2}\/\d{2}\/\d{4}$/.test('22/22/2222'))
+
+// var dia = (/^(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/)
+// document.write(dia.test('32'))
+// document.write(dia.test('31'))
+
+// var mes = (/^(0[1-9]|1[0-2])$/)
+// document.write(mes.test('12'))
+// document.write(mes.test('13'))
+
+// var ano = (/^\d{4}$/)
+// document.write(ano.test('24'))
+// document.write(ano.test('2024'))
+
+// var data = (/^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/)
+// document.write(data.test('22/04/2120'))
+
+// var cpf = (/^([0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2})$/)
+// document.write(cpf.test('957.877.555-55'))
+
